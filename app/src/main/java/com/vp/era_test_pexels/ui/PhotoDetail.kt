a97b8bd4899cd14c3160a594bbbef2a0cc21f928
Phuc Doan <phucdoankaizen@gmail.com>
2025-05-06 10:08:06 +0700

    ok

--- app/src/main/java/com/vp/era_test_pexels/ui/PhotoDetail.kt
+++ app/src/main/java/com/vp/era_test_pexels/ui/PhotoDetail.kt
@@ -4,49 +4,31 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.toSize
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
-import com.vp.era_test_pexels.R
 import com.vp.era_test_pexels.ui.main.SharedTopBar
-import kotlin.math.max
 
 class PhotoDetail : ComponentActivity() {
 
@@ -85,30 +67,60 @@ fun ZoomableImageScreen(imageUrl: String) {
     var scale by remember { mutableStateOf(1f) }
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
+    // container size, we use fillMaxSize() then it's just the screen size
+    var containerSize by remember { mutableStateOf(IntSize.Zero) }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.Black)
+            // get container size to calculate limit to X (horizontal)
+            .onSizeChanged { containerSize = it }
+            // double-tap to reset scale and offsets
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onDoubleTap = {
+                        scale = 1f
+                        offsetX = 0f
+                        offsetY = 0f
+                    }
+                )
+            }
+            //  pinch-to-zoom và pan
             .pointerInput(Unit) {
                 detectTransformGestures { _, pan, zoom, _ ->
-                    scale = max(1f, scale * zoom)
-                    offsetX += pan.x
+                    //  scale limit update from 1x to 5x
+                    scale = (scale * zoom).coerceIn(1f, 5f)
+
+                    //  fillMaxSize() and scale = 1 then image fit to container,
+                    // calculate :
+                    // maxOffsetX = (containerWidth * (scale - 1)) / 2
+                    val maxOffsetX = if (scale > 1f) (containerSize.width * (scale - 1f)) / 2f else 0f
+                    offsetX = (offsetX + pan.x).coerceIn(-maxOffsetX, maxOffsetX)
+
+                    // Y : vertical is free
                     offsetY += pan.y
                 }
             }
+            //  scale and translation for image
             .graphicsLayer(
-                scaleX = scale, scaleY = scale,
-                translationX = offsetX, translationY = offsetY
+                scaleX = scale,
+                scaleY = scale,
+                translationX = offsetX,
+                translationY = offsetY
             )
     ) {
         Image(
-            painter = rememberAsyncImagePainter(imageUrl),
+            painter = rememberAsyncImagePainter(model = imageUrl),
             contentDescription = null,
             contentScale = ContentScale.Fit,
             modifier = Modifier.fillMaxSize()
         )
     }
 }
+
+
+
 
 
 
